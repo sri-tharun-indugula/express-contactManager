638f645371d1240646e6fad5349c9b74b73ca28b
sri-tarun-indugula <indugulasritharun123@gmail.com>
2024-02-27 09:49:28 +0530

    added error handler

--- middleware/errorhandler.js
+++ middleware/errorhandler.js
@@ -1,7 +1,24 @@
-const errorHandler=(err,req,res,next)=>{
-    const statusCode=res.statusCode ? res.statusCode:500;
-    console.log("111",err.message)
-    res.json({message:err.message,code:statusCode})
+const { constants } = require('../constant')
+const errorHandler = (err, req, res, next) => {
+    const statusCode = res.statusCode ? res.statusCode : 500;
+    console.log("111", err.message)
+    res.json({ message: err.message, code: statusCode })
+    switch (statusCode) {
+        case constants.VALIDATION_ERROR:
+            res.json({ title: 'valadation failed', message: err.message, stackTrace: err.stack })
+            break;
+        case constants.NOT_FOUND:
+            res.json({ title: 'Not Found', message: err.message, stackTrace: err.stack })
+            break;
+        case constants.FORBIDDEN:
+            res.json({ title: 'Forbidden values', message: err.message, stackTrace: err.stack })
+            break;
+        case constants.UNAUTHORISED:
+            res.json({ title: 'Unauthorised', message: err.message, stackTrace: err.stack })
+            break;
+        default:
+            break;
+    }
 };
 
-module.exports=errorHandler
+module.exports = errorHandler
