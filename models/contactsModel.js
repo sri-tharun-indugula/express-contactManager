7f447717e723ac527529a4e896e58c00415ab8a4
sri-tarun-indugula <indugulasritharun123@gmail.com>
2024-03-13 10:51:02 +0530

    added the mongoosemodel schema

---
+++ models/contactsModel.js
@@ -0,0 +1,18 @@
+const mongoose=require('mongoose')
+
+const contactSchema=mongoose.Schema({
+    name:{
+        type:String,
+        required:[true,'Please enter the name']
+    },email:{
+        type:String,
+        required:[true,'Please enter the email']
+    },phone:{
+        type:String,
+        required:[true,'Please enter the phone']
+    }
+},{
+    timestamps:true
+})
+
+module.exports=mongoose.model("Contacts",contactSchema)
